ddfb0e5f8a3a558cd4536d031a3052d774c64e54
edustadler <eduardostadlerlabbo@gmail.com>
2022-12-23 01:45:56 -0300

    just created a simple loop

--- single.php
+++ single.php
@@ -1,3 +1,17 @@
 <?php get_header(); ?>
 
+<?php
+if (have_posts()) :
+    while (have_posts()) : the_post();
+        // My loop is here
+        the_title();
+        the_content();
+    endwhile;
+
+else :
+// if I haven't post...
+endif;
+?>
+
+
 <?php get_footer(); ?>
